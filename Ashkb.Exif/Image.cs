d45cd91f7cc15c19cb1aca16ca1f3d572b4d0f22
Ashish Boobun <ashish.boobun@aranzmedical.com>
2018-02-09 16:37:02 +1300

    Move validation of Jpeg file into Utility class

--- Ashkb.Exif/Image.cs
+++ Ashkb.Exif/Image.cs
@@ -1,8 +1,7 @@
-﻿using Ashkb.Exif.Model;
-using System;
-using System.IO;
+﻿using System.IO;
 using Ashkb.Exif.Enums;
 using Ashkb.Exif.Error;
+using Ashkb.Exif.Model;
 
 namespace Ashkb.Exif
 {
@@ -22,12 +21,7 @@ namespace Ashkb.Exif
 
     public static ImageInfo GetImageInfo(Stream imageStream)
     {
-      imageStream.Seek(0, SeekOrigin.Begin);
-
-      byte[] header = new byte[2];
-      imageStream.Read(header, 0, header.Length);
-
-      if (Utility.Combine(header[0], header[1]) == (int)JpegMarker.StartOfImage)
+      if (Utility.IsJpeg(imageStream))
       {
         int p = 1;
         //TODO parse jpeg
--- Ashkb.Exif/Utility.cs
+++ Ashkb.Exif/Utility.cs
@@ -1,4 +1,7 @@
-﻿namespace Ashkb.Exif
+﻿using System.IO;
+using Ashkb.Exif.Enums;
+
+namespace Ashkb.Exif
 {
     public static class Utility
     {
@@ -7,6 +10,23 @@
         
         int combined = b1 << 8 | b2;
         return combined;
+      }
+
+      public static bool IsJpeg(Stream stream)
+      {
+        byte[] header = new byte[2];
+        byte[] ending = new byte[2];
+
+        stream.Seek(0, SeekOrigin.Begin);
+        stream.Read(header, 0, header.Length);
+
+        stream.Seek(stream.Length - 2, SeekOrigin.Begin);
+        stream.Read(ending, 0, ending.Length);
+
+        var isJpeg = Combine(header[0], header[1]) == (int)JpegMarker.StartOfImage &&
+                     Combine(ending[0], ending[1]) == (int)JpegMarker.EndOfImage;
+
+        return isJpeg;
       }
     }
 }
