7b69555e2f35b90c65bc6b5d57b704ce5fd8b67f
Ashish Boobun <ashish.boobun@aranzmedical.com>
2018-02-18 23:56:55 +1300

    Parse ImageFileDirectory and entry arrays

---
+++ Ashkb.Exif/IfdEntry.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Ashkb.Exif.Enums;
+
+namespace Ashkb.Exif
+{
+  public class IfdEntry
+  {
+    private const int IfdEntryValueByteLength = 4;
+
+    private IfdEntry(ushort tag, ushort type, uint count, byte[] value)
+    {
+      Tag = tag;
+      Type = type;
+      Count = count;
+      Value = value;
+    }
+
+    public ushort Tag { get; }
+
+    public ushort Type { get; }
+
+    public uint Count { get; }
+
+    public byte[] Value { get; }
+
+    public static IfdEntry ParseFromByte(byte[] imageData, int startIndex, ByteOrder byteOrder)
+    {
+      var typeValue =
+        BitConverterExtension.ToUInt16(imageData, startIndex + 2, byteOrder, BitConverterExtension.SystemByteOrder);
+      if (!typeof(FieldType).IsEnumDefined(typeValue))
+      {
+        throw new ArgumentOutOfRangeException("Invalid field type value");
+      }
+
+      var tag = BitConverterExtension.ToUInt16(imageData, startIndex, byteOrder, BitConverterExtension.SystemByteOrder);
+      var count = BitConverterExtension.ToUInt32(imageData, startIndex + 4, byteOrder,
+        BitConverterExtension.SystemByteOrder);
+      var value = BitConverterExtension.CopyArrayEndianness(imageData, startIndex + 8, byteOrder,
+        BitConverterExtension.SystemByteOrder,
+        IfdEntryValueByteLength);
+      var valueLength = FieldTypeHelper.GetFieldTypeByteLength((FieldType) typeValue) * (int) count;
+
+      if (valueLength > IfdEntryValueByteLength)
+      {
+        var valueOffset = BitConverterExtension.ToUInt32(value, 0, byteOrder, BitConverterExtension.SystemByteOrder);
+        value = BitConverterExtension.CopyArrayEndianness(imageData, (int) valueOffset, byteOrder,
+          BitConverterExtension.SystemByteOrder,
+          valueLength);
+      }
+
+      return new IfdEntry(tag, typeValue, count, value);
+    }
+  }
+}
---
+++ Ashkb.Exif/ImageFileDirectory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Ashkb.Exif.Enums;
+
+namespace Ashkb.Exif
+{
+  public class ImageFileDirectory
+  {
+    private static readonly ushort NumberOfFieldsByteLength = 2;
+
+    private static readonly ushort IfdEntryByteLength = 12;
+    public ushort NumberOfIfdEntries { get; set; }
+
+    public uint NextIfdOffset { get; set; }
+
+    public List<IfdEntry> IfdEntries { get; set; }
+
+
+    public static ImageFileDirectory ParseFromImageData(byte[] imageData, int startIndex, ByteOrder byteOrder)
+    {
+      var ifd = new ImageFileDirectory
+      {
+        NumberOfIfdEntries =
+          BitConverterExtension.ToUInt16(imageData, startIndex, byteOrder, BitConverterExtension.SystemByteOrder)
+      };
+
+      ifd.IfdEntries = new List<IfdEntry>(ifd.NumberOfIfdEntries);
+
+      for (var i = 0; i < ifd.NumberOfIfdEntries; i++)
+      {
+        var ifdEntry = IfdEntry.ParseFromByte(imageData, startIndex + NumberOfFieldsByteLength + IfdEntryByteLength * i,
+          byteOrder);
+        ifd.IfdEntries.Add(ifdEntry);
+      }
+
+      ifd.NextIfdOffset = BitConverterExtension.ToUInt32(imageData,
+        startIndex + NumberOfFieldsByteLength + IfdEntryByteLength * ifd.NumberOfIfdEntries, byteOrder,
+        BitConverterExtension.SystemByteOrder);
+
+      return ifd;
+    }
+  }
+}
