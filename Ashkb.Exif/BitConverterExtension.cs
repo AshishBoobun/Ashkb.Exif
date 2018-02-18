5a4f38a81b1df7b2a2dbb50ca710674b7412f855
Ashish Boobun <ashish.boobun@aranzmedical.com>
2018-02-18 23:37:10 +1300

    Refactor

--- Ashkb.Exif/BitConverterExtension.cs
+++ Ashkb.Exif/BitConverterExtension.cs
@@ -10,26 +10,36 @@ namespace Ashkb.Exif
 
     public static uint ToUInt32(byte[] value, int startIndex, ByteOrder from, ByteOrder to)
     {
-      var valueEndianess = CheckEndianessValue(value, startIndex, from, to, 4);
-      return BitConverter.ToUInt32(valueEndianess, 0);
+      var valueEndianness = CopyArrayEndianness(value, startIndex, from, to, 4);
+      return BitConverter.ToUInt32(valueEndianness, 0);
     }
 
     public static ushort ToUInt16(byte[] value, int startIndex, ByteOrder from, ByteOrder to)
     {
-      var valueEndianess = CheckEndianessValue(value, startIndex, from, to, 2);
-      return BitConverter.ToUInt16(valueEndianess, 0);
+      var valueEndianness = CopyArrayEndianness(value, startIndex, from, to, 2);
+      return BitConverter.ToUInt16(valueEndianness, 0);
     }
 
-    private static byte[] CheckEndianessValue(byte[] value, int startIndex, ByteOrder from, ByteOrder to, int length)
+    /// <summary>
+    ///   Copies a range of elements from an System.Array starting at the specified source index and pastes them to a new
+    ///   System.Array. The length and the indexes are specified as 32-bit integers.
+    /// </summary>
+    /// <param name="value">The System.Array that contains the data to copy.</param>
+    /// <param name="startIndex">A 32-bit integer that represents the index in the sourceArray at which copying begins.</param>
+    /// <param name="from">The endianness order in which the <paramref name="value" /> is arranged </param>
+    /// <param name="to">The endianness order in which to copy the value into a new array</param>
+    /// <param name="length">A 32-bit integer that represents the number of elements to copy.</param>
+    /// <returns>new System.Array that contain copied data</returns>
+    public static byte[] CopyArrayEndianness(byte[] value, int startIndex, ByteOrder from, ByteOrder to, int length)
     {
-      var valueEndianess = new byte[length];
-      Array.Copy(value, startIndex, valueEndianess, 0, length);
+      var valueEndianness = new byte[length];
+      Array.Copy(value, startIndex, valueEndianness, 0, length);
       if (from != to)
       {
-        Array.Reverse(valueEndianess);
+        Array.Reverse(valueEndianness);
       }
 
-      return valueEndianess;
+      return valueEndianness;
     }
   }
 }
